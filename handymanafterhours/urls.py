1102a147b4904297011766f3db78bbe107427b8c
Agata <agata.kinga@gmail.com>
2017-05-20 21:41:18 +0200

    page layout, navbar according to pattern

--- handymanafterhours/urls.py
+++ handymanafterhours/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from handymanafterhours import views
+
 urlpatterns = [
+    url(r'^$', views.HomePage.as_view()),
+
     url(r'^admin/', admin.site.urls),
 ]
